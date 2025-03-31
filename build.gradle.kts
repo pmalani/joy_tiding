3ba39706e31d3ba17c6973fe897eb5885d840f42
praka <p@m.org>
2025-03-30 20:48:49 -0700

    added logging example

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.slf4j:slf4j-api:2.0.17")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
---
+++ src/main/java/k/Logging.java
@@ -0,0 +1,31 @@
+package k;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SuppressWarnings("unused")
+public class Logging {
+
+    private static final Logger logger = LoggerFactory.getLogger(Logging.class);
+
+    void dontConcatenate(String name) {
+        logger.info("processing " + name);
+    }
+
+    void missingPlaceholders(String name) {
+        logger.info("processing", name);
+    }
+
+    void extraPlaceholders(String name) {
+        logger.info("processing {} {}", name);
+    }
+
+    void logException(String name) {
+        try {
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+            logger.warn("interrupted while processing {}", name, e);
+        }
+    }
+
+}
